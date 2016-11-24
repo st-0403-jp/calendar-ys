d1e496bd45967f6223a98e17f7c109267067a707
st-0403-jp <sato252011@gmail.com>
2016-11-24 17:41:14 +0900

    【add】top.jsちょっと整理

--- src/js/top.js
+++ src/js/top.js
@@ -1,7 +1,9 @@
 /* top.js */
 globalPage.top = function () {
+    // 現在のyyyymmを取得
     var nowYearMonth = globalNowYear + globalNowMonth;
 
+    // セレクトメニューを現在のyyyymmにする
     var yearMonthOptions = $('#select-year-month').find('option');
     $.each(yearMonthOptions, function (i, option) {
         var option = $(option);
@@ -11,6 +13,7 @@ globalPage.top = function () {
         }
     });
 
+    // セレクトメニューでカレンダーを取得
     $('#select-year-month').on('change', function () {
         var selectYearMonth = nowYearMonth;
         $.each(yearMonthOptions, function (i, option) {
@@ -25,26 +28,12 @@ globalPage.top = function () {
                     contentType: 'application/json',
                     dataType: 'json'
                 }, function (resData) {
-                    //setTimeout(function () {
-                        globalCommon.util.renderHandlebars('#calendar-template', '#calendar', resData);
-                    //}, 2000);
+                    globalCommon.util.renderHandlebars('#calendar-template', '#calendar', resData);
                 }, function (resErr) {
                     console.log(resErr.responseText);
                 });
     });
-
-    var calendarApiUrl = '/api/' + nowYearMonth + '/date.json';
-    globalCommon.util.requestCalendar({
-                type: 'GET',
-                url: calendarApiUrl,
-                contentType: 'application/json',
-                dataType: 'json'
-            }, function (resData) {
-                //setTimeout(function () {
-                    globalCommon.util.renderHandlebars('#calendar-template', '#calendar', resData);
-                //}, 2000);
-            }, function (resErr) {
-                console.log(resErr.responseText);
-            });
+    // 初期表示のカレンダーをレンダリング
+    $('#select-year-month').trigger('change');
 };
 globalPage.top();
